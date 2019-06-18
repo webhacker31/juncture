34e9fb375b501a417abf7413f0cb85f10bced224
Nino Zabate <nzabate@expertcollege.com>
2019-06-18 17:16:12 +0800

    Bug fixes for downlines

--- wp-content/themes/twentynineteen-child/Objects/UserData.php
+++ wp-content/themes/twentynineteen-child/Objects/UserData.php
@@ -109,7 +109,7 @@ class User_Data {
         $stack_geneology = [];
 
         foreach ( $data_obj as $data_key ) {
-            
+
             array_push( $stack_geneology, array( ( $data_key->user_dl_left_id == "" ) ? strval( ++$available_counter ) : $data_key->user_dl_left_id, $parent_user_id, ( $data_key->user_dl_left_id == "" ) ? "Available" :  $data_key->user_dl_left_id ) );
             array_push( $stack_geneology, array( ( $data_key->user_dl_right_id == "" ) ? strval( ++$available_counter ) : $data_key->user_dl_right_id, $parent_user_id, ( $data_key->user_dl_right_id == "" ) ? "Available" :  $data_key->user_dl_right_id ) );
 
@@ -129,7 +129,7 @@ class User_Data {
         $main_user_downline = $this->get_user_downline_by_id( $user_id );
 
         foreach ( $this->stack_geneology( $main_user_downline, $user_id, $available_counter ) as $data_key ) {
-                
+
             array_push( $user_downlines, $data_key );
 
         }
@@ -137,16 +137,16 @@ class User_Data {
         array_push( $users_to_check, ( $main_user_downline[0]->user_dl_left_id == "" ) ? "Available" : $main_user_downline[0]->user_dl_left_id );
         array_push( $users_to_check, ( $main_user_downline[0]->user_dl_right_id == "" ) ? "Available" : $main_user_downline[0]->user_dl_right_id );
 
-        for ( $index = 0; $index < count( $users_to_check ) - 1; $index++ ) { 
+        for ( $index = 0; $index < count( $users_to_check ); $index++ ) { 
 
             if ( $users_to_check[ $index ] != "Available" ) {
 
                 $the_users_downline = $this->get_user_downline_by_id( $users_to_check[ $index ] );
 
                 foreach ( $this->stack_geneology( $the_users_downline, $users_to_check[ $index ], $available_counter ) as $data_key ) {
-                
+
                     array_push( $user_downlines, $data_key );
-                
+
                 }
 
                 array_push( $users_to_check, ( $the_users_downline[0]->user_dl_left_id == "" ) ? "Available" : $the_users_downline[0]->user_dl_left_id );
