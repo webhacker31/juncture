8cbac6e76b7b678c842db7c9291f758bcf80dcb2
Nino Zabate <nzabate@expertcollege.com>
2019-07-05 11:06:28 +0800

    Replace folder name for Object to obj; Move old request/ files to temp/

---
+++ wp-content/themes/twentynineteen-child/obj/UserData.php
@@ -0,0 +1,313 @@
+<?php
+
+class User_Data {
+
+    private $wpdb;
+
+    public function __construct() {
+
+        global $wpdb;
+        $this->wpdb = $wpdb;
+
+    }
+
+    public function get_all_users() {
+
+        $get_user_info = $this->wpdb->get_results( "SELECT * FROM j_users_info" );
+
+        return $get_user_info;
+
+    }
+
+    public function get_user_info_by_id( $user_id ) {
+
+        $get_user_info_by_id = $this->wpdb->get_results( "SELECT * FROM j_users_info WHERE user_info_id='{$user_id}'" );
+
+        return $get_user_info_by_id;
+
+    }
+
+    public function get_username_by_id( $user_id ) {
+
+        $get_username_by_id = $this->wpdb->get_var( "SELECT user_username FROM j_users_info WHERE user_info_id='{$user_id}'" );
+
+        return $get_username_by_id;
+        
+    }
+
+    public function get_user_info_by_credentials( $username, $password ) {
+
+        $get_user_info_by_credentials = $this->wpdb->get_results( "SELECT * FROM j_users_info WHERE user_info_id='{$username}' AND user_password='{$password}' OR user_username='{$username}' AND user_password='{$password}'" );
+
+        return $get_user_info_by_credentials;
+
+    }
+
+    public function get_users_transactions() {
+
+        $get_user_transaction = $this->wpdb->get_results( "SELECT * FROM j_users_transactions" );
+
+        foreach ( $get_user_transaction as &$data ) {
+
+            $data->user_info_id = $data->user_info_id . ' - ' . strtoupper( $this->get_username_by_id( $data->user_info_id ) );
+
+        }
+
+        return $get_user_transaction;
+
+    }
+
+    public function get_user_transaction_by_id( $user_id ) {
+
+        $get_user_transaction_by_id = $this->wpdb->get_results( "SELECT * FROM j_users_transactions WHERE user_info_id='{$user_id}'" );
+
+        return $get_user_transaction_by_id;
+
+    }
+
+    public function get_users_withdrawal_status() {
+
+        $get_user_withdrawal_status = $this->wpdb->get_results( "SELECT * FROM j_users_withdrawal_status" );
+
+        return $get_user_withdrawal_status;
+
+    }
+
+    public function get_user_withdrawal_status_by_id( $user_id ) {
+
+        $get_user_withdrawal_status_by_id = $this->wpdb->get_results( "SELECT * FROM j_users_withdrawal_status WHERE user_info_id='{$user_id}'" );
+
+        return $get_user_withdrawal_status_by_id;
+
+    }
+
+    public function get_user_earnings( $user_id ) {
+
+        $get_user_earnings = $this->wpdb->get_results( "SELECT * FROM j_users_earnings WHERE user_info_id='{$user_id}'" );
+        
+        foreach ( $get_user_earnings as &$data ) {
+
+            $data->earning_pair_left = $data->earning_pair_left . ' - ' . strtoupper( $this->get_username_by_id( $data->earning_pair_left ) );
+            $data->earning_pair_right = $data->earning_pair_right . ' - ' . strtoupper( $this->get_username_by_id( $data->earning_pair_right ) );
+
+        }
+
+        return $get_user_earnings;
+
+    }
+
+    public function get_user_downline_by_id( $user_id ) {
+
+        $get_user_downline_by_id = $this->wpdb->get_results( "SELECT user_dl_left_id, user_dl_right_id FROM j_users_info WHERE user_info_id='{$user_id}'" );
+
+        return $get_user_downline_by_id;
+
+    }
+
+    public function stack_geneology( $data_obj, $parent_user_id, &$available_counter ) {
+
+        $stack_geneology = [];
+
+        foreach ( $data_obj as $data_key ) {
+
+            array_push( $stack_geneology, array( ( $data_key->user_dl_left_id == "" ) ? strval( ++$available_counter ) : $data_key->user_dl_left_id, $parent_user_id, ( $data_key->user_dl_left_id == "" ) ? "Available" :  $data_key->user_dl_left_id ) );
+            array_push( $stack_geneology, array( ( $data_key->user_dl_right_id == "" ) ? strval( ++$available_counter ) : $data_key->user_dl_right_id, $parent_user_id, ( $data_key->user_dl_right_id == "" ) ? "Available" :  $data_key->user_dl_right_id ) );
+
+        }
+
+        return $stack_geneology;
+
+    }
+
+    public function get_user_geneology_by_id( $user_id, $return_type = '' ) {
+
+        $user_downlines = [];
+        $users_to_check = [];
+        $counter = 0;
+        $available_counter = 0;
+
+        $main_user_downline = $this->get_user_downline_by_id( $user_id );
+
+        foreach ( $this->stack_geneology( $main_user_downline, $user_id, $available_counter ) as $data_key ) {
+
+            array_push( $user_downlines, $data_key );
+
+        }
+
+        array_push( $users_to_check, ( $main_user_downline[0]->user_dl_left_id == "" ) ? "Available" : $main_user_downline[0]->user_dl_left_id );
+        array_push( $users_to_check, ( $main_user_downline[0]->user_dl_right_id == "" ) ? "Available" : $main_user_downline[0]->user_dl_right_id );
+
+        for( $index = 0; $index < count( $users_to_check ); $index++ ) {
+
+            if( count( $user_downlines ) >= 21 ) break;
+
+            if( $users_to_check[ $index ] != "Available" ) {
+
+                $the_users_downline = $this->get_user_downline_by_id( $users_to_check[ $index ] );
+
+                foreach ( $this->stack_geneology( $the_users_downline, $users_to_check[ $index ], $available_counter ) as $data_key ) {
+
+                    array_push( $user_downlines, $data_key );
+
+                }
+
+                array_push( $users_to_check, ( $the_users_downline[0]->user_dl_left_id == "" ) ? "Available" : $the_users_downline[0]->user_dl_left_id );
+                array_push( $users_to_check, ( $the_users_downline[0]->user_dl_right_id == "" ) ? "Available" : $the_users_downline[0]->user_dl_right_id );
+
+            }
+
+        }
+
+        array_unshift( $user_downlines, array( $user_id, '', $user_id ) );
+
+        switch( strtolower( $return_type ) ) {
+
+            case "id":
+
+                return $user_downlines;
+                break;
+
+            case "username":
+
+                foreach ( $user_downlines as &$downline_group ) {
+
+                    if ( $downline_group[2] != "Available" ) {
+                    
+                        $downline_group[2] = $this->get_username_by_id( $downline_group[2] );
+
+                    }
+
+                }
+
+                return $user_downlines;
+                break;
+            
+            default:
+                
+                return $user_downlines;
+                break;
+
+        }
+
+    }
+
+    public function get_user_pairing_by_id( $user_id, $purpose = '' ) {
+
+        $temp_pairing_stack = array();
+        $pairing_check = array();
+        $pairing_success = array();
+        $data_counter = 2;
+        $prev_data_counter = 0;
+        $available_counter = 0;
+
+        $user_geneology_data = $this->get_user_geneology_by_id( $user_id );
+
+        for( $index = 1; $index < count( $user_geneology_data ); ) {
+
+            if( $index <= $data_counter ) {
+
+                array_push( $temp_pairing_stack, $user_geneology_data[ $index ][2] );
+
+                if( $user_geneology_data[ $index ][2] == "Available" ) $available_counter++;
+
+                $index++;
+
+            }
+            
+            if( $index > $data_counter ) {
+
+                array_push( $pairing_check, $temp_pairing_stack );
+
+                unset( $temp_pairing_stack );
+                $temp_pairing_stack = array();
+                $data_counter_holder = $data_counter;
+                $data_counter = $data_counter + ( ( ( $data_counter - $prev_data_counter ) * 2 ) - ( $available_counter * 2 ) );
+                $prev_data_counter = $data_counter_holder;
+                $available_counter = 0;
+
+            }
+
+        }
+
+        foreach( $pairing_check as $pairing_group ) {
+
+            $pairing_group_count = count( $pairing_group );
+            $pairing_group_half_count = $pairing_group_count / 2;
+            $available_counter = 0;
+
+            for( $index = $pairing_group_count; $index > $pairing_group_half_count; $index-- ) {
+
+                if( $pairing_group[ $index - 1 ] == 'Available' ) $available_counter++;
+
+            }
+
+            if( $available_counter == $pairing_group_half_count ) break;
+
+            for( $index = 0; $index < $pairing_group_count; $index++ ) {
+
+                if( $pairing_group[ $index ] != "Available" && $pairing_group[ $pairing_group_count - ( $index + 1 ) ] != "Available") {
+
+                    $is_pairing_exist = $this->wpdb->get_var( 'SELECT COUNT(*) FROM j_users_earnings WHERE user_info_id="' . $user_id . '" AND earning_pair_left="' . $pairing_group[ $index ] . '" OR user_info_id="' . $user_id . '" AND earning_pair_right="' . $pairing_group[ $index ] . '"' );
+
+                    if( ! $is_pairing_exist ) {
+
+                        array_push( $pairing_success, array( $pairing_group[ $index ], $pairing_group[ $pairing_group_count - ( $index + 1 ) ] ) );
+
+                    }
+
+                }
+
+                if( $index + 1 == $pairing_group_count / 2 ) {
+
+                    break;
+
+                }
+
+            }
+
+        }
+
+        if( $purpose == 'request_withdrawal' ) {
+
+            return $pairing_success;
+
+        } else {
+
+            return $this->get_user_pairing_obj_format( $pairing_success );
+
+        }
+
+    }
+
+    public function get_user_pairing_obj_format( $pairing_array_format ) {
+
+        $pairing_to_obj = [];
+
+        foreach ( $pairing_array_format as $pairing_group ) {
+
+            array_push( $pairing_to_obj, [
+                'earning_type' => 'Pairing',
+                'earning_pair_left_id' => $pairing_group[0],
+                'earning_pair_right_id' => $pairing_group[1],
+                'earning_pair_left' => $pairing_group[0] . ' - ' . $this->get_username_by_id( $pairing_group[0] ),
+                'earning_pair_right' => $pairing_group[1] . ' - ' . $this->get_username_by_id( $pairing_group[1] ),
+                'earning_amount' => '500'
+            ]);
+
+        }
+
+        return $pairing_to_obj;
+
+    }
+
+    public function get_main_settings() {
+
+        $get_main_settings = $this->wpdb->get_results( "SELECT * FROM j_main_settings" );
+
+        return $get_main_settings;
+
+    }
+
+}
+
+$User_Data = new User_Data;
---
+++ wp-content/themes/twentynineteen-child/temp/request/login.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Login Response
+ * Method: POST
+ * 
+ */
+
+require_once ( '../../../../wp-load.php' );
+require_once ( '../obj/UserData.php' );
+
+if ( isset( $_POST[ 'username' ] ) && isset( $_POST[ 'password' ] ) ) {
+
+    $username = $_POST[ 'username' ];
+    $password = $_POST[ 'password' ];
+
+    $status = [];
+
+    $user_data_info = $User_Data->get_user_info_by_credentials( $username, $password );
+
+    if ( $user_data_info != NULL ) {
+
+        $url = esc_url( home_url( '/' ) ) . 'wp-content/themes/twentynineteen-child/request/session_start.php';
+
+        $fields = [
+            'request_type'      => 'session',
+            'user_id'           => $user_data_info[0]->user_info_id,
+            'user_username'     => $user_data_info[0]->user_username,
+            'user_password'     => $user_data_info[0]->user_password,
+            'user_role'         => $user_data_info[0]->user_role,
+            'user_upline_id'    => $user_data_info[0]->user_upline_id,
+            'user_position'     => $user_data_info[0]->user_position
+        ];
+
+        $fields_string = http_build_query( $fields );
+
+        $ch = curl_init();
+
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_POST, count( $fields ) );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_string );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); 
+
+        // Result
+        echo curl_exec( $ch );
+
+    } else {
+
+        $status = [
+            'status' => 'failed',
+            'message' => 'User not exist.'
+        ];
+
+    }
+
+} else { // If direct access
+
+    header('Location: ' . esc_url( home_url( '/' ) ) . 'page-error/?error=direct_access');
+    die();
+
+}
