4c06440de747fd3589fa07f5c52b70c753b4affc
Niño Joevel Zabate <ninojoevelz@gmail.com>
2019-07-12 15:41:39 +0800

    Resolved Conflicts; Remove delete button on Admin (#20)

--- wp-content/themes/twentynineteen-child/obj/UserAction.php
+++ wp-content/themes/twentynineteen-child/obj/UserAction.php
@@ -202,29 +202,6 @@ class User_Action {
 
     }
 
-    public function delete_user( $user_id ) {
-
-        if( $this->is_user_has_downline( $user_id ) ) {
-
-            $this->wpdb->delete( 'j_users', array( 'user_id' => $user_id ) );
-            $this->wpdb->delete( 'j_users_info', array( 'user_info_id' => $user_id ) );
-
-            return [
-                'status' => 'Success',
-                'message' => 'User data deleted.'
-            ];
-
-        } else {
-
-            return [
-                'status' => 'Failed',
-                'message' => 'Deletion of user cannot be process.'
-            ];
-
-        }
-
-    }
-
     public function update_user( $user_data ) {
 
         $this->wpdb->update( 
